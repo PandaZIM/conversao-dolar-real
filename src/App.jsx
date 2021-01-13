b93b6639efb84c0b73c3b6d9254f429fb99b1fef
pandazim <jaotoledo74@gmail.com>
2021-01-12 20:46:36 -0400

    Manuseamento de estado dos botoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  // eslint-disable-next-line no-unused-vars
+  const [valorDolar, setValorDolar] = useState();
+  // eslint-disable-next-line no-unused-vars
+  const [taxaEstado, setTaxaEstado] = useState();
+  // eslint-disable-next-line no-unused-vars
+  const [opcaoPagamento, setOpcaoPagamento] = useState();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-principal">
+      <form className="page-principal-form" action="">
+        <span>Digita o valor em Dólar: </span>
+        <input className="page-principal-input-text"
+          type="text"
+          placeholder="US$"
+          onChange={e => setValorDolar(e.target.value)}/>
+        <span>Taxa do estado: </span>
+        <input type="text"
+          placeholder="%"
+          onChange={e => setTaxaEstado(e.target.value)}/>
+        <input className="page-principal-input-radio"
+          type="radio" 
+          value="Dinheiro"
+          onChange={e => setOpcaoPagamento(e.target.value)}/> Dinheiro
+        <input className="page-principal-input-radio" 
+          type="radio"
+          value="Cartao"
+          onChange={e => setOpcaoPagamento(e.target.value)}/> Cartão
+      </form>
     </div>
   );
 }
