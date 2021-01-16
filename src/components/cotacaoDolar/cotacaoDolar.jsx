26a45f0f95b9d12a6595eb168e515b1166eaa28e
pandazim <jaotoledo74@gmail.com>
2021-01-16 11:32:01 -0400

    Finalizado componente

--- src/components/cotacaoDolar/cotacaoDolar.jsx
+++ src/components/cotacaoDolar/cotacaoDolar.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
-
-const [cotacaoDolar, setCotacaoDolar] = useState();
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api/api';
 
 function CotacaoDolar() {
+  const [cotacaoDolar, setCotacaoDolar] = useState();
+
+  //consumação da api
+  useEffect(() => {
+    api.get('https://economia.awesomeapi.com.br/json/all')
+    .then(response => {
+        const cotacao = response.data
+        setCotacaoDolar(cotacao)
+    })
+    .catch(error => {
+      //gestão de erros
+      console.log(error)
+    })
+  },[]);
+
+  if(cotacaoDolar === undefined) {
+    return <> </> 
+  }
+
   return(
-    
+    <div className="page-principal-cotacoes">
+      <span>Cotação do dia: <p>R${cotacaoDolar.USD.high}</p></span>
+    </div>
   );
 }
 
