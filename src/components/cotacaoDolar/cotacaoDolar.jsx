ae11ef366ef302d9a20cb2d119d5495b7ab0d248
pandazim <jaotoledo74@gmail.com>
2021-01-16 10:10:35 -0400

    Criação do componente

---
+++ src/components/cotacaoDolar/cotacaoDolar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const [cotacaoDolar, setCotacaoDolar] = useState();
+
+function CotacaoDolar() {
+  return(
+    
+  );
+}
+
+export default CotacaoDolar;
