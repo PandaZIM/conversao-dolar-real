d569366aa5061fff120367e9fe1af31727042242
pandazim <jaotoledo74@gmail.com>
2021-01-18 21:17:51 -0400

    Criação do componente

---
+++ src/components/TotalEmDinheiro/TotalEmDinheiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TotalEmDinheiro() {
+  return (
+        <div>
+            <span>Total sem imposto em real: </span>
+            <span>Total com imposto em real: </span>
+        </div>
+  );
+}
+
+export default TotalEmDinheiro;
