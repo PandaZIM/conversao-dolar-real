8296f1528302a82e6a3149a25e0ce02cd5bba5c3
pandazim <jaotoledo74@gmail.com>
2021-01-21 22:48:23 -0400

    Mudança das regras de negocio

--- src/components/inputsForm/inputsForm.jsx
+++ src/components/inputsForm/inputsForm.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-duplicate-props */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, {useState} from 'react';
@@ -10,39 +12,50 @@ function InputsForm(props) {
     }
 
     const [valorDolar, setValorDolar] = useState(0);
+    console.log(valorDolar)
+
     const [taxaEstado, setTaxaEstado] = useState(0);
 
-
+    //Regra de negócio
     const IOFDinheiro = props.valor * 0.011
-    const IOFCartao = props.valor * 0.064
+    const IOFCartao = isNaN(valorDolar * 0.064) ? 0 : valorDolar * 0.064
+    const impostoEstado = (valorDolar * (taxaEstado/100))
     
-    const operacaoSemImposto = parseFloat((valorDolar * (taxaEstado/100))) + parseFloat(valorDolar)
-    const operacaoComImposto = parseFloat((((valorDolar * (taxaEstado/100))) + parseFloat(valorDolar)) + parseFloat((IOFDinheiro) + parseFloat(props.valor)))
-    const operacaoComImpostoCartao = parseFloat((((valorDolar * (taxaEstado/100))) + parseFloat(valorDolar)) + parseFloat((IOFCartao) + parseFloat(props.valor)))
+    const operacaoSemImposto = isNaN(parseFloat(valorDolar)) ? 0 : parseFloat(valorDolar)
+    const operacaoComImposto = isNaN(parseFloat(valorDolar + impostoEstado + IOFDinheiro)) ? 0 : parseFloat(valorDolar + impostoEstado + IOFDinheiro)
+    const operacaoSemImpostoCartao = isNaN(parseFloat(valorDolar)) ? 0 : parseFloat(valorDolar)
+    const operacaoComImpostoCartao = isNaN(parseFloat(valorDolar + impostoEstado + IOFCartao)) ? 0 : parseFloat(valorDolar + impostoEstado + IOFCartao)
+
     const operacaoSemImpostoReal = operacaoSemImposto * parseFloat(props.valor)
     const operacaoComImpostoReal = operacaoComImposto * parseFloat(props.valor)
+    const operacaoSemImpostoCartaoReal = operacaoSemImpostoCartao * parseFloat(props.valor)
     const operacaoComImpostoCartaoReal = operacaoComImpostoCartao * parseFloat(props.valor)
+    
 
   return(
     <form className="page-principal-form" action="">
-        <span>Digita o valor em Dólar: </span>
+        <span>Digite o valor em Dólar: </span>
         <input className="page-principal-input-text"
-        type="text"
+        type="number"
         placeholder="US$"
-        onChange={e => setValorDolar(parseInt(e.target.value))}/>
+        onChange={e => setValorDolar(parseInt(e.target.value))} />
         <span>Taxa do estado: </span>
-        <input type="text"
+        <input className="page-principal-input-text"
+        type="number"
         placeholder="%"
-        onChange={e => setTaxaEstado(parseInt(e.target.value))}/>
+        onChange={(e) => {
+          setTaxaEstado(parseInt(e.target.value))
+        }}/>
         <br/>
         <input className="page-principal-input-radio"
         type="radio" 
         value="Dinheiro"
         name="OpcaoPagamento"
-        onChange={(e) => 
+        onChange={() => 
         {   
-            props.taxa(IOFDinheiro)
-            props.onSemImposto(`$${operacaoSemImposto}`)
+            
+            props.taxa(IOFDinheiro.toFixed(2))
+            props.onSemImposto(`$${operacaoSemImposto.toFixed(2)}`)
             props.onComImposto(`$${operacaoComImposto.toFixed(2)}`)
             props.onSemImpostoReal(`R$${operacaoSemImpostoReal.toFixed(2)}`)
             props.onComImpostoReal(`R$${operacaoComImpostoReal.toFixed(2)}`)
@@ -51,12 +64,12 @@ function InputsForm(props) {
         type="radio" 
         value="Cartao"
         name="OpcaoPagamento"
-        onChange={(e) => 
+        onChange={() => 
         {   
-            props.taxa(IOFCartao)
-            props.onSemImposto(`$${operacaoSemImposto}`)
+            props.taxa(IOFCartao.toFixed(2))
+            props.onSemImposto(`$${operacaoSemImpostoCartao.toFixed(2)}`)
             props.onComImposto(`$${operacaoComImpostoCartao.toFixed(2)}`)
-            props.onSemImpostoReal(`R$${operacaoSemImpostoReal.toFixed(2)}`)
+            props.onSemImpostoReal(`R$${operacaoSemImpostoCartaoReal.toFixed(2)}`)
             props.onComImpostoReal(`R$${operacaoComImpostoCartaoReal.toFixed(2)}`)
         }}/> Cartão
     </form>
