69f2bf878de3d63401062b78626a6b27155a1bdc
pandazim <jaotoledo74@gmail.com>
2021-02-15 22:58:06 -0400

    Criação do componente

---
+++ src/components/ButtonConverter/ButtonConverter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function ButtonConverter() {
+  return (
+      <div>
+          <img src="" alt="Botao de conversão"/>
+          <button>Converter</button>
+      </div>
+  );
+}
+
+export default ButtonConverter;
